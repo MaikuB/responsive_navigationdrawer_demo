406d73340f59f692820682483c37fbf0d8bb943f
Michael Bui <25263378+MaikuB@users.noreply.github.com>
2019-09-29 00:24:05 +1000

    prefix all route names with forward slash as per convention to allow going to route directly (e.g. on the web)

--- lib/constants/route_names.dart
+++ lib/constants/route_names.dart
@@ -1,6 +1,6 @@
 class RouteNames {
   static const String home = '/';
-  static const String gallery = 'gallery';
-  static const String slideshow = 'slideshow';
-  static const String settings = 'settings';
+  static const String gallery = '/gallery';
+  static const String slideshow = '/slideshow';
+  static const String settings = '/settings';
 }
